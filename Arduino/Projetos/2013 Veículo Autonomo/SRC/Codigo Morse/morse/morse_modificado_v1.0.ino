011cffd5080792b010fdbeb1a57f109d2a370446
fgbettio@gmail.com <fgbettio@gmail.com@0bbe6709-ab84-18a3-8e30-468e91dc0343>
2013-10-15 12:49:55 +0000

    renomeando arquivos

---
+++ Arduino/Projetos/2013 Veículo Autonomo/SRC/Codigo Morse/morse/morse_modificado_v1.0.ino
@@ -0,0 +1,79 @@
+int pin = 8;
+int codigo=12;
+int vetbin[8];
+int cont;
+void setup()
+{
+ Serial.begin(9600);
+  pinMode(pin, OUTPUT);
+}
+
+
+
+void loop()
+{
+  dot(); dot(); dot();
+  dash(); dash(); dash();
+  dot(); dot(); dot();
+  dec2bin(codigo);
+  delay(3000);
+  
+}
+
+void dot()
+{
+  digitalWrite(pin, HIGH);
+  delay(250);
+  digitalWrite(pin, LOW);
+  delay(250);
+}
+
+void dash()
+{
+  digitalWrite(pin, HIGH);
+  delay(1000);
+  digitalWrite(pin, LOW);
+  delay(250);
+}
+
+
+void dec2bin(int valor)
+  {
+    int resto;
+      for (cont=7; cont<=0;cont--)
+      {
+        if (valor >=1)
+        {
+            resto = valor %2;
+            vetbin[cont]=resto;
+            Serial.print(resto);
+            valor = (valor /2)
+              if (valor == 1)
+              {
+                vetbin[cont]=valor;
+              }
+                           
+          }
+          
+        else 
+         {   
+           
+           /// parei aqui, falta preencher o resto do vetor com 000000
+            Serial.print(valor);
+            vetbin[cont]=valor;
+          }
+      }     
+  
+        vetbin[3]=0;
+        vetbin[2]=0;
+        vetbin[1]=0;
+        vetbin[0]=0;
+        Serial.println();
+        
+    for (int i=0;i<=7;i++)
+    {
+      Serial.print (vetbin[i]);
+      
+    }
+    Serial.println();
+  }
